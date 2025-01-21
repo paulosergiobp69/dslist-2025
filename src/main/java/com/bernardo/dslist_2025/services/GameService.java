1bc0a8da370c8996c4a842322deb5c7f782ec88d
paulosergio <paulosergio@cidadesinteligentes.org.br>
2025-01-21 15:21:49 -0300

    AJUSTE SERVICE

--- src/main/java/com/bernardo/dslist_2025/services/GameService.java
+++ src/main/java/com/bernardo/dslist_2025/services/GameService.java
@@ -21,6 +21,7 @@ public class GameService {
 	public List<GameMinDTO> findAll(){
 		// cria variavel tipo list game
 		List<Game> result = gameRepository.findAll();
+		// convertendo o retorno da tabela completa game para gameDTO
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 		
