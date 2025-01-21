288eaef0472ef8c6b9e37e138721792cc8ea7eba
paulosergio <paulosergio@cidadesinteligentes.org.br>
2025-01-21 09:47:47 -0300

    PRIMEIRO COMMIT

---
+++ src/main/java/com/bernardo/dslist_2025/Dslist2025Application.java
@@ -0,0 +1,13 @@
+package com.bernardo.dslist_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dslist2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dslist2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bernardo/dslist_2025/Dslist2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernardo.dslist_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dslist2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
